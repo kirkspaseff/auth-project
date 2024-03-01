bb06374a8f14cbde9a77031de9ab5037a6b8d7a1
Kirk <kirk@getpixeltag.com>
2024-03-01 15:07:10 -0800

    installed dependencies, created new next project

---
+++ auth-project/src/app/app.ts
@@ -0,0 +1,9 @@
+import { Provider } from "next-auth/client";
+
+function App({ Component, pageProps }) {
+    return (<Provider session={pageProps.session}>
+        <Component {...pageProps} /> 
+    </Provider>)
+}
+
+export default App;
